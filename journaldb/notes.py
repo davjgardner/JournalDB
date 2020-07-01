e10d35ce9de419fb9897f2595e0e281732c4b995
davjgardner <davjgardner@gmail.com>
2020-07-01 19:32:10 -0400

    Add input form to top of main view

--- journaldb/notes.py
+++ journaldb/notes.py
@@ -11,18 +11,15 @@ from journaldb.db import get_db
 
 bp = Blueprint('notes', __name__)
 
-@bp.route('/compose', methods=('GET', 'POST'))
-def compose():
-    if request.method == 'POST':
-        date = request.form['date']
-        body = request.form['body']
-        db = get_db()
-        error = None
+def add_entry(date, body):
+    db = get_db()
+    error = None
 
-        if not date:
-            error = 'Date is required.'
-        elif not body:
-            error = 'Entry cannot be empty.'
+    if not date:
+        error = 'Date is required.'
+    elif not body:
+        error = 'Entry cannot be empty.'
+    else:
         e = db.execute('SELECT id FROM journal WHERE date = ?', (date,)).fetchone()
         if e is not None:
             error = 'Date {} is already recorded. Edit?'.format(date)
@@ -30,21 +27,34 @@ def compose():
             # TODO: save typed body text, show it in the edit screen
             return redirect(url_for('notes.edit', id=e['id']))
 
-        if error is None:
-            weekday = calendar.day_name[curr_date.weekday()]
-            db.execute('INSERT INTO journal (date, weekday, body) VALUES (?, ?, ?)', (date, weekday, body))
-            db.commit()
-            return redirect(url_for('notes.index'))
+    if error is None:
+        weekday = calendar.day_name[datetime.date.fromisoformat(date).weekday()]
+        db.execute('INSERT INTO journal (date, weekday, body) VALUES (?, ?, ?)', (date, weekday, body))
+        db.commit()
+        return redirect(url_for('notes.index'))
 
-        flash(error)
+    flash(error)
 
     return render_template('compose.html')
 
-@bp.route('/')
+
+@bp.route('/compose', methods=('GET', 'POST'))
+def compose():
+    if request.method == 'POST':
+        date = request.form['date']
+        body = request.form['body']
+        return add_entry(date, body)
+
+@bp.route('/', methods=('GET', 'POST'))
 def index():
     db = get_db()
+    if request.method == 'POST':
+        date = request.form['date']
+        body = request.form['body']
+        return add_entry(date, body)
+
     entries = db.execute('SELECT id, date, weekday, body FROM journal ORDER BY date DESC').fetchall()
-    return render_template('view.html', entries=entries)
+    return render_template('view.html', entries=entries, date=datetime.date.today())
 
 @bp.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
@@ -65,7 +75,7 @@ def edit(id):
             error = 'Entry cannot be empty.'
 
         if error is None:
-            weekday = weekday = calendar.day_name[curr_date.weekday()]
+            weekday = weekday = calendar.day_name[datetime.date.fromisoformat(date).weekday()]
             db.execute('UPDATE journal SET date = ?, weekday = ?, body = ? WHERE id = ?', (date, weekday, body, id))
             db.commit()
             return redirect(url_for('notes.index'))
