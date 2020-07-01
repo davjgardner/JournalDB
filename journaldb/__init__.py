dbc2ce1bb89ff4675de065bc45e497b5ba14532c
davjgardner <davjgardner@gmail.com>
2020-07-01 09:35:35 -0400

    Initialize database

--- journaldb/__init__.py
+++ journaldb/__init__.py
@@ -20,6 +20,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    from . import db
+    db.init_app(app)
+
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
